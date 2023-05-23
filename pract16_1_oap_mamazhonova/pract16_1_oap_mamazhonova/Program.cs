2fa9a72cb1ff93b0d8b450111136519b466daf1c
mddchn <93264667+mddchn@users.noreply.github.com>
2023-05-23 15:50:45 +0500

    Add files via upload

---
+++ pract16_1_oap_mamazhonova/pract16_1_oap_mamazhonova/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace pract16_1_oap_mamazhonova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            { 
+                Console.WriteLine("Введите слово для поиска");
+                string word = Console.ReadLine()?.ToLower();
+                if (string.IsNullOrWhiteSpace(word))
+                {
+                    Console.WriteLine("Введено пустое значение");
+                    return;
+                }
+                string text = File.ReadAllText("text.txt").ToLower();
+                int count = text.Split(' ', '.', ',', ';', ':', '!', '?', '\n', '\r').Count(w => w == word);
+                Console.WriteLine($"Были найдены {count} вхождений поискового запроса: \"{word}\"");
+            }
+           
+        }
+    }
+}
